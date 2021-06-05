70215db140dc1e8044d4f7466c2160fcb2183e05
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-05 11:52:53 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -125,21 +125,18 @@ async def auto_filter(bot, update):
                 ]
             )
         
-    else:
-        Send_message = await bot.send_message(
+    else: 
+         Send_message = await bot.send_message(
          chat_id=update.chat.id,
-         text="<b>Couldn't Find This Movie☹️ Try Again ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>",
-         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="❕CHANNEL❕", url="https://t.me/joinchat/lNPUvWyEcW5jYjE9")]]),
-         parse_mode="html",
-         disable_web_page_preview=True,
+         text="<b>Couldn't Find This Movie☹️ Try Again ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>", 
          reply_to_message_id=update.message_id
         )
-         await asyncio.sleep(30)
+         await asyncio.sleep(10)
          await Send_message.delete()
     
 
     if len(results) == 0: # double check
-        return
+        return   #module by shamilhabeebnelli
     
     else:
     
