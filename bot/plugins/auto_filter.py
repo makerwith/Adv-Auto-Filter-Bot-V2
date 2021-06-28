e543e4233c7a00843806ed164ed30cd2eff92a05
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-28 21:17:41 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_name}" "{file_size}"
+            button_text = f"{file_name}" + "{file_size}"
             
 
             if file_type == "video":
@@ -134,7 +134,7 @@ async def auto_filter(bot, update):
          disable_web_page_preview=True,
          reply_to_message_id=update.message_id
         )
-         await asyncio.sleep(10)
+         await asyncio.sleep(60)
          await Send_message.delete()
     
 
@@ -212,7 +212,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"**Found {(len_results)} Results For Your Query:** <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
