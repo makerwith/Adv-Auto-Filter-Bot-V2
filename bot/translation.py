96c0fb24b24d8a9171c3881f1f6fc8b99f6222fc
makerwith <83757763+makerwith@users.noreply.github.com>
2021-07-18 21:09:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -9,7 +9,7 @@ Am Just A Advance Auto Filter Bot....😉
 
 You Can't Use Me.I Specially Maded For My Group.
 
- 🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <a href="https://t.me/Vivek_Kerala">ᴠɪᴠᴇᴋ ⚡</a></b>"""    
+ 🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <a href="https://t.me/Makerwith">IRFAN 💻</a></b>"""    
     
     HELP_TEXT = """
 **My Owners Can Only See This 😋**
@@ -17,9 +17,11 @@ You Can't Use Me.I Specially Maded For My Group.
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/Vivek_Kerala">Vivek</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Makerwith">Vivek</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+
 """
