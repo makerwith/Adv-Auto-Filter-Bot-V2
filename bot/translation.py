7b11b8b03498db76de529c9dc8a50b5af98be4dc
makerwith <83757763+makerwith@users.noreply.github.com>
2021-07-21 19:51:28 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -23,5 +23,5 @@ You Can't Use Me.I Specially Maded For My Group.
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <b><i><a href="https://t.me/joinchat/eZk8OSmHwR5jYTJl">Get Me</a></i></b>
+<b>➥ Source Code</b> : <b><i><a href="https://t.me/joinchat/eZk8OSmHwR5jYTJl">Click Here</a></i></b>
 """
